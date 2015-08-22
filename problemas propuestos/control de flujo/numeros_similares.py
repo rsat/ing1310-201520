d0495b800db015ac86f5a4be1769d4727b1e4995
Ricardo Sat <rsat89@gmail.com>
2015-08-22 19:13:51 -0300

    ejercicio propuestos

---
+++ problemas propuestos/control de flujo/numeros_similares.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+ing1310-201520 22/08/2015 Ricardo Sat
+
+Realice un programa que le pida tres números enteros a un usuario e imprima en consola True
+o False, dependiendo si los números son similares entre ellos. Sean a, b y divisor tres números enteros,
+a y b serán similares si y sólo si, divisor divide a ambos o divisor no divide a ninguno de los dos.
+Al final de su programa deberá preguntarle al usuario si desea comprobar si otros números son similares
+
+Ejemplo:
+
+Ingrese a: 10
+Ingrese b: 5
+Ingrese divisor: 2
+False
+Desea comprobar si otros números son similares(s/n)? s
+
+Ingrese a: 10
+Ingrese b: 15
+Ingrese divisor: 5
+True
+Desea comprobar si otros números son similares(s/n)? ...
+...
+
+
+
+"""
---
+++ problemas propuestos/control de flujo/suma_digitos.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+ing1310-201520 22/08/2015 Ricardo Sat
+
+Realice un programa que reciba un número entero e imprima en consola la suma de sus dígitos. Al final de su
+programa deberá preguntarle al usuario si quiere calcular la suma de los dígitos de otro número.
+
+Ejemplo
+
+Ingrese un número: 111
+La suma de los dígitos de 111 es 3
+Desea calcular la suma de los dígitos de otro número(s/n)? s
+Ingrese un número: 942
+La suma de los dígitos de 942 es 15
+Desea calcular la suma de los dígitos de otro número(s/n)? ...
+...
+
+"""
